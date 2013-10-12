197f6dd70f6cffcde08f5e0a80444ec45cb7f0b8
Hugo <hwainwright@gmx.com>
2013-10-12 01:35:59 +0100

    -- read from patch file rather than piping to stdin

--- src/main/java/com/frugs/filesync/local/system/SystemCommandExecutor.java
+++ src/main/java/com/frugs/filesync/local/system/SystemCommandExecutor.java
@@ -28,7 +28,7 @@ public class SystemCommandExecutor {
 
     public void gitApply(String diffFile) throws IOException {
         logger.info("applying diff");
-        execute("patch -p1 <" + diffFile);
+        execute("patch -p1 -i " + diffFile);
     }
 
     private InputStream execute(String command) throws IOException {
