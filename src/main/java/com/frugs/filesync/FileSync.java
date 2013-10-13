2219a1342dba30b956c7cf27e9a31412a5eb0a50
Hugo <hwainwright@gmx.com>
2013-10-13 15:50:45 +0100

    -- optimised imports

--- src/main/java/com/frugs/filesync/FileSync.java
+++ src/main/java/com/frugs/filesync/FileSync.java
@@ -1,14 +1,5 @@
 package com.frugs.filesync;
 
-import com.frugs.filesync.local.FileUpdateFacade;
-import com.frugs.filesync.local.LocalFileUpdatePollingService;
-import com.frugs.filesync.local.LocalFileUpdater;
-import com.frugs.filesync.local.LockingDiff;
-import com.frugs.filesync.local.system.FileWriter;
-import com.frugs.filesync.local.system.SystemCommandExecutor;
-import com.frugs.filesync.local.system.SystemCommandFacade;
-import com.frugs.filesync.remote.RemoteFileUpdateReceiver;
-import com.frugs.filesync.remote.RemoteFileUpdateSender;
 import com.frugs.filesync.task.PollLocalUpdatesTask;
 
 import java.net.InetAddress;
@@ -16,7 +7,6 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 import static com.frugs.filesync.FileSyncModule.createModule;
 import static java.lang.Integer.parseInt;
